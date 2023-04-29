f231a4fb0fd9ff4ccefbc4456b627b1e00d48b70
Gatuno7000 <107892696+Jose-AE@users.noreply.github.com>
2023-04-29 13:31:50 -0600

    removed gitignore dist

---
+++ server/dist/data/domainArtifacts.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DOMAIN_ARTIFACTS = void 0;
+exports.DOMAIN_ARTIFACTS = [
+    ["gladiators_finale", "wanderers_troupe"],
+    ["thundering_fury", "thundersoother"],
+    ["viridescent_venerer", "maiden_beloved"],
+    ["archaic_petra", "retracing_bolide"],
+    ["crimson_witch_of_flames", "lavawalker"],
+    ["bloodstained_chivalry", "noblesse_oblige"],
+    ["blizzard_strayer", "heart_of_depth"],
+    ["tenacity_of_the_millelith", "pale_flame"],
+    ["shimenawas_reminiscence", "emblem_of_severed_fate"],
+    ["husk_of_opulent_dreams", "ocean_hued_clam"],
+    ["vermillion_hereafter", "echoes_of_an_offering"],
+    ["deepwood_memories", "gilded_dreams"],
+    ["desert_pavilion_chronicle", "flower_of_paradise_lost"],
+    ["vourukashas_glow", "nymphs_dream"],
+];
+//# sourceMappingURL=domainArtifacts.js.map
