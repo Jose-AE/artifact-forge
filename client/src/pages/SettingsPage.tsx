eb266a9bd18c6165487efbecde87ecf981a965a0
Gatuno7000 <107892696+Jose-AE@users.noreply.github.com>
2023-04-29 13:58:01 -0600

    test (nothing added)

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react";
 import { LoginContext } from "../App";
 import { Avatar, Button, Text } from "@chakra-ui/react";
-
+//
 export default function SettingsPage() {
   const { loggedUser, setLoggedUser } = useContext(LoginContext);
 
